640a6979b4893df95fa0594fa7c4636bbc632b31
jwkim <jwkim@hanyang.ac.kr>
2025-03-10 19:43:07 +0900

    bank_class checkout

--- Hello/hi.cpp
+++ Hello/hi.cpp
@@ -1,30 +1,6 @@
 #include <iostream>
-#include <string>  // <cstring> 대신 <string> 사용
+#include <string>
 using namespace std;
-
-class Rectangle
-{
-private:
-	int width;
-	int height;
-
-public:
-	Rectangle(int a, int b) 
-	{
-		width = a;
-		height = b;
-	}
-
-	int get_area()
-	{
-		return width * height;
-	}
-	int get_parameter()
-	{
-		return 2 * (width + height);
-	}
-	
-};
 
 class BankAccount
 {
