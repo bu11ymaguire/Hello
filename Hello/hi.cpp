f9b2dcaf6ecf825f6ff0eb6776524252b103b863
jwkim <jwkim@hanyang.ac.kr>
2025-03-10 09:43:56 +0900

    djfuqek

--- Hello/hi.cpp
+++ Hello/hi.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include <iostream>	
+#include <cstring>
 using namespace std;
 
-int main() {
-	cout << "HaHa" << endl;
+const int kMaxStr = 100;
+
+int main()
+{
+	char args1[] = "Hello World!";
+	char* args2 = new char[kMaxStr];
+	strcpy_s(args2, (strlen(args1) + 1), args1);
+	cout << args1 << endl;
+	cout << args2 << endl;
+	delete[] args2;
 	return 0;
 }
