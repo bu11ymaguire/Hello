f46a08a2dab74ba6ab3b21952dd69f321935f97a
jwkim <jwkim@hanyang.ac.kr>
2025-03-10 10:49:00 +0900

    djfuqEK djfuwnj

--- Hello/hi.cpp
+++ Hello/hi.cpp
@@ -1,31 +1,30 @@
-#include <iostream>	
-#include <cstring>
+#include <iostream>
+#include <string>  // <cstring> 대신 <string> 사용
 using namespace std;
 
-struct info
-{
-	char name[100];
-	int age;
-	int grade;
+struct info {
+    string name;
+    int age;
+    int grade;
 };
 
-int main()
-{
-	info students[3];
-	students[0].name = "John";
-	students[0].age = 20;
-	students[0].grade = 11;
+int main() {
+    info students[3];
 
-	students[1].name = "Kylian";
-	students[1].age = 12;
-	students[1].grade = 6;
+    students[0].name = "John";   // 문자열 직접 할당
+    students[0].age = 20;
+    students[0].grade = 11;
 
-	students[2].name = "Lionel";
-	students[2].age = 15;
-	students[2].grade = 8;
+    students[1].name = "Kylian";
+    students[1].age = 12;
+    students[1].grade = 6;
 
-	double avg = (students[0].age + students[1].age + students[2].age)/3;
-	cout << "Average age: " << avg << endl;
+    students[2].name = "Lionel";
+    students[2].age = 15;
+    students[2].grade = 8;
 
-	return 0;
+    double avg = (students[0].age + students[1].age + students[2].age) / 3.0;  // 3.0으로 나눠야 정확한 실수형 결과
+    cout << "Average age: " << avg << endl;
+
+    return 0;
 }
